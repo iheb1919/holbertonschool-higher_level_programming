6b7bf1731b7fa29c78b56ba03da345a0be883831
iheb1919 <ihebmejri14@gmail.com>
2019-12-10 16:32:39 +0100

    ll

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-x = abs(number) % 10
+x = number % 10
 stupidmessage = " and is less than 6 and not 0 "
 if x > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, x))
