0d239b145736143b5a3dc3427514be5a8044e113
iheb1919 <ihebmejri14@gmail.com>
2019-12-10 17:08:51 +0100

    ll

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, - 1):
     j = i
-    if i % 2 != 0:
-       j = i - 32
-    else:
-        print("{}".format(chr(j)), end="")
+    if j % 2 != 0:
+        j = j - 32
+    print("{}".format(chr(j)), end="")
