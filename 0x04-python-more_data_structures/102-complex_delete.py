3e42ca013f4f2b36448853c5f502b2776cc06611
iheb1919 <ihebmejri14@gmail.com>
2019-12-19 23:44:20 +0100

    ff

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@ def complex_delete(a_dictionary, value):
     while value in a_dictionary.values():
         for i, j in a_dictionary.items():
             if j == value:
-                del a_dictionary[k]
+                del a_dictionary[i]
                 break
 
     return (a_dictionary)
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
