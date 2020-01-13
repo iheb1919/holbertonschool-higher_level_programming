92f8ff1782127c6052bf381e3f0f742781e11a90
iheb1919 <ihebmejri14@gmail.com>
2020-01-13 23:54:26 +0100

    dd

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,10 @@
+def print_square(size):
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif type(size) == int(size) and size < 0:
+        raise TypeError("size must be an integer")
+    elif size > 0:
+        print(('#' * size + '\n') * size, end="")
