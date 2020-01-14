3168c6b882152216740bd53a73df8e62f12cc602
iheb1919 <ihebmejri14@gmail.com>
2020-01-14 12:26:56 +0100

    ff

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
-
+    first = False
+    last = False
+    if first_name is None and last_name is None:
+        raise TypeError("missing 1 required positional argument: 'first_name'")
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')
     if type(last_name) is not str:
