7684e4f9a233d7c57d479b2e16ad70c4679dc75a
iheb1919 <ihebmejri14@gmail.com>
2020-01-14 14:21:30 +0100

    ff

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-"""
-This is the "0-add_integer" module.
-
-The 0-add_integer module supplies one function, add_integer(). For example,
-
->>> add_integer(1,2)
-3
-"""
-
 
 def add_integer(a, b=98):
 
