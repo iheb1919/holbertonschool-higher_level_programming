360a4b441b6cc2455775548483bda0da2f63cee4
iheb1919 <ihebmejri14@gmail.com>
2020-01-14 14:23:19 +0100

    ff

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+This is the "0-add_integer" module
+The 0-add_integer module supplies one function, add_integer(). For example,
+>>> add_integer(1,2)
+3
+"""
+
 
 def add_integer(a, b=98):
-
-
+    """ Returns the sum of two int/float parameters. """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     elif type(a) == float:
