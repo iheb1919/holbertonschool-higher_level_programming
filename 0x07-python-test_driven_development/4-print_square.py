7dca059c5260dcbde041e015d338dc2aca4ecc62
iheb1919 <ihebmejri14@gmail.com>
2020-01-20 21:40:13 +0100

    ff

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,19 +13,16 @@ The example module supplies one function, print_sauare(size).  For example,
 
 
 def print_square(size):
-"""
-return a sauare
-"""
+    """ print a sauare. """
 
     if type(size) is not int:
         raise TypeError("size must be an integer")
     elif size < 0:
-        raise ValueError("size must be >= 0")
-    elif type(size) == int(size) and size < 0:
-        raise TypeError("size must be an integer")
+        raise ValueError("size must be >= 0")    
     elif size > 0:
         print(('#' * size + '\n') * size, end="")
 
 
 if __name__ == "__main__":
-    import doctest.testfile("./tests/4-print_saquare.txt")
+    import doctest
+    doctest.testfile("./tests/4-print_saquare.txt")
