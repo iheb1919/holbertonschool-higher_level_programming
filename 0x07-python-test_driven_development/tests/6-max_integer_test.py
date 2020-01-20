ce36ddfb2115d09c370407038d1fe19c470de9be
iheb1919 <ihebmejri14@gmail.com>
2020-01-20 23:52:40 +0100

    ff

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,9 +7,9 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max_last(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-
-    def test_max_first(self):
-        self.assertEqual(max_integer([0, 13, 552, 9]), 552)
+    
+    def test_max_one_elem(self):
+        self.assertEqual(max_integer([44]), 44)
 
     def test_max_mid(self):
         self.assertEqual(max_integer([1, 9]), 9)
@@ -20,8 +20,6 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_all_negative(self):
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-    def test_max_one_elem(self):
-        self.assertEqual(max_integer([44]), 44)
 
     def test_max_empty_list(self):
         self.assertEqual(max_integer([]), None)
