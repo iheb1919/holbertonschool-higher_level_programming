182142553af69187fdac439a51111c8af2bb9492
iheb1919 <ihebmejri14@gmail.com>
2020-03-03 22:19:22 +0100

    dd

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,5 @@
---script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT a.name FROM tv_genres a, tv_show_genres b, tv_shows c
-WHERE a.id = b.genre_id AND b.show_id = c.id AND c.title = 'Dexter'
-ORDER BY a.name ASC;
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
