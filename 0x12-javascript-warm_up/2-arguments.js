4b33ed07c528bdeb4483428e9b8e2a3087d3acbc
iheb1919 <ihebmejri14@gmail.com>
2020-05-05 21:00:27 +0100

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length <= 2) {
     p = ' No Arguments';}
-else if (process.argv.length >= 3) {
+else if (process.argv.length == 3) {
     p = 'Arguments found';}
 else {
     p = 'Arguments founds';}
