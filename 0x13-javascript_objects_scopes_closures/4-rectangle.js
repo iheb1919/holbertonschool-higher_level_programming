5938791c7a0a4e03aec367f7230e62ee3c4ce85b
iheb1919 <ihebmejri14@gmail.com>
2020-05-12 00:43:16 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,11 +7,13 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate (width, height) {
     const x = this.width;
     this.width = this.height;
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,7 +4,7 @@
 exports.esrever = function (list) {
   const lis2 = [];
   for (let i = list.length; i > -1; i--) {
-  lis2.push(list[i]);
+    lis2.push(list[i]);
   }
   return lis2;
 };
