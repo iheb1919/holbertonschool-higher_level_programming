c68e96cc87ee0ad6f17b39473090f9fc5d97bbf1
iheb1919 <ihebmejri14@gmail.com>
2020-05-12 00:53:31 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 
 exports.esrever = function (list) {
   const lis2 = [];
-  for (let i = list.length; i > -1; i--) {
+  for (let i = list.length - 1; i > -1; i--) {
     lis2.push(list[i]);
   }
   return lis2;
