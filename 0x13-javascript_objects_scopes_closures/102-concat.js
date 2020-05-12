f26df658a44362b27fb36beff16116bc44d72ab1
iheb1919 <ihebmejri14@gmail.com>
2020-05-12 03:52:44 +0100

    try

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,12 @@
-let en  = new Filer=Reader();
-en.onload = function(){
-    process.argv[i].textContent = this.result;
+#!/usr/bin/node
+let i = 2;
+while (i <= 3) {
+  const fs = require('fs');
+  fs.readFile(process.argv[i], (err, data) => {
+    if (err) throw err;
+    fs.appendFile(process.argv[4], data, (err) => {
+      if (err) throw err;
+    });
+  });
+  i++;
 }
-en.readAsText(this.files[0]);
