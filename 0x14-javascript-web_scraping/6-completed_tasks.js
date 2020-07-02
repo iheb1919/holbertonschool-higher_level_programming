1450bcd1a437a74ef2c4702d560ce23defa1bb86
iheb1919 <ihebmejri14@gmail.com>
2020-07-02 01:56:20 +0100

    ff

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,28 @@
 #!/usr/bin/node
-const request = require('request');
-let c = 0;
-const dict = {};
-request(process.argv[2], function (error, body, response) {
-  if (error) {
-    console.error('error:', error);
+// status of a request
+const myRequest = require('request');
+const myUrl = process.argv[2];
+myRequest(myUrl, function (err, res, body) {
+  if (err) {
+    console.log(err);
   } else {
-    body = JSON.parse(response);
-    let userid = body[0].userId;
-    for (let i = 0; i < body.length; i++) {
-      if (userid !== body[i].userId) {
-        dict[userid.toString()] = c;
-        userid = body[i].userId;
-        c = 0;
-      }
-      if (body[i].completed === true) {
-        c++;
+    let count = 0;
+    const dict1 = {};
+    const jsonBody = JSON.parse(body);
+    if (jsonBody != null) {
+      let userant = jsonBody[0].userId;
+      for (let j = 1; j < jsonBody.length; j++) {
+        for (let i = 0; i < jsonBody.length; i++) {
+          const user = jsonBody[i].userId;
+          if (jsonBody[i].completed === true && userant === user) {
+            count = count + 1;
+            dict1[jsonBody[i].userId] = count;
+          }
+        }
+        userant = jsonBody[j].userId;
+        count = 0;
       }
     }
-    dict[userid.toString()] = c;
-    console.log(dict);
+    console.log(dict1);
   }
 });
